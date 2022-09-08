dec7be66c47ef118fcc1d6661821d23a114de371
Mas Jc <113080604+masjc@users.noreply.github.com>
2022-09-08 16:52:57 +0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,5 +1,3 @@
-<script>
-//<![CDATA[
 let s = ['//pl17086594.highperformancegate.com/b0/e0/f7/b0e0f7aef3540e5e4e5fbe40b4d9e002.js'];
 s.forEach((a) => {
     let sc = document.createElement('script');
@@ -7,5 +5,3 @@ s.forEach((a) => {
     sc.setAttribute('src', a);
     document.querySelector('head').appendChild(sc);
 })
-//]]>
-</script>
